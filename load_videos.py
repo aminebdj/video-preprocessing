62eb6760d3945bda5e5663d6ebb8711d87832709
Mohamed El Amine Boudjoghra <95423980+aminebdj@users.noreply.github.com>
2023-07-06 15:28:58 +0400

    updated

--- load_videos.py
+++ load_videos.py
@@ -6,25 +6,24 @@ import subprocess
 from multiprocessing import Pool
 from itertools import cycle
 import warnings
-import glob
-import time
+import cv2
 from tqdm import tqdm
 from util import save
 from argparse import ArgumentParser
 from skimage import img_as_ubyte
 from skimage.transform import resize
+import torchaudio
 warnings.filterwarnings("ignore")
 
 DEVNULL = open(os.devnull, 'wb')
 
 def download(video_id, args):
     video_path = os.path.join(args.video_folder, video_id + ".mp4")
-    subprocess.call([args.youtube, '-f', "''best/mp4''", '--write-auto-sub', '--write-sub',
-                     '--sub-lang', 'en', '--skip-unavailable-fragments',
+    subprocess.call([args.youtube, '-f', "''best/mp4''", '--write-auto-subs', '--write-subs',
+                     '--sub-langs', 'en', '--skip-unavailable-fragments',
                      "https://www.youtube.com/watch?v=" + video_id, "--output",
                      video_path], stdout=DEVNULL, stderr=DEVNULL)
     return video_path
-
 
 def run(data):
     video_id, args = data
@@ -34,22 +33,36 @@ def run(data):
     if not os.path.exists(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')):
        print ('Can not load video %s, broken link' % video_id.split('#')[0])
        return 
-    reader = imageio.get_reader(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4'))
-    fps = reader.get_meta_data()['fps']
+    # reader = imageio.get_reader(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4'))
+    pth = os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')
+    cap = cv2.VideoCapture(pth)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    video_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # get the audio tensor
+    # try:
+    audio_tensor, samplerate = torchaudio.load(pth)
+    audio_frames = audio_tensor.shape[1]
+    # except:
+    #     print("Error loading audio for video %s" % video_id)
+    #     return
 
     df = pd.read_csv(args.metadata)
     df = df[df['video_id'] == video_id]
     
     all_chunks_dict = [{'start': df['start'].iloc[j], 'end': df['end'].iloc[j],
-                        'bbox': list(map(int, df['bbox'].iloc[j].split('-'))), 'frames':[]} for j in range(df.shape[0])]
-    ref_fps = df['fps'].iloc[0]
+                        'bbox': list(map(int, df['bbox'].iloc[j].split('-'))), 'frames':[], 'audio': []} for j in range(df.shape[0])]
+
     ref_height = df['height'].iloc[0]
     ref_width = df['width'].iloc[0]
     partition = df['partition'].iloc[0]
+    i = 0
     try:
-        for i, frame in enumerate(reader):
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
             for entry in all_chunks_dict:
-                if (i * ref_fps >= entry['start'] * fps) and (i * ref_fps < entry['end'] * fps):
+                if (i * fps >= entry['start'] * fps) and (i * fps < entry['end'] * fps):
                     left, top, right, bot = entry['bbox']
                     left = int(left / (ref_width / frame.shape[1]))
                     top = int(top / (ref_height / frame.shape[0]))
@@ -59,6 +72,8 @@ def run(data):
                     if args.image_shape is not None:
                        crop = img_as_ubyte(resize(crop, args.image_shape, anti_aliasing=True))
                     entry['frames'].append(crop)
+            i += 1
+        cap.release()
     except imageio.core.format.CannotReadFrameError:
         None
 
@@ -69,19 +84,23 @@ def run(data):
             first_part = ""
         first_part = first_part + '#'.join(video_id.split('#')[::-1])
         path = first_part + '#' + str(entry['start']).zfill(6) + '#' + str(entry['end']).zfill(6) + '.mp4'
-        save(os.path.join(args.out_folder, partition, path), entry['frames'], args.format)
+        start_audio, end_audio = int((entry['start'] / video_frames) * audio_frames), int((entry['end'] / video_frames) * audio_frames)
+        subclip_audio = audio_tensor[:,int(start_audio):int(end_audio)]
+        save(os.path.join(args.out_folder, partition, path), entry['frames'], subclip_audio, args.format, fps, samplerate, args.desired_fps, args.desired_sample_rate)
+        
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--video_folder", default='youtube-taichi', help='Path to youtube videos')
-    parser.add_argument("--metadata", default='taichi-metadata-new.csv', help='Path to metadata')
+    parser.add_argument("--metadata", default='taichi-metadata.csv', help='Path to metadata')
     parser.add_argument("--out_folder", default='taichi-png', help='Path to output')
     parser.add_argument("--format", default='.png', help='Storing format')
     parser.add_argument("--workers", default=1, type=int, help='Number of workers')
-    parser.add_argument("--youtube", default='./youtube-dl', help='Path to youtube-dl')
- 
-    parser.add_argument("--image_shape", default=(256, 256), type=lambda x: tuple(map(int, x.split(','))),
+    parser.add_argument("--youtube", default='yt-dlp', help='Path to youtube-dl')
+    parser.add_argument("--desired_fps", default=25, type=int, help='Desired FPS')
+    parser.add_argument("--desired_sample_rate", default=16000, type=int, help='Desired sample rate')
+    parser.add_argument("--image_shape", default=(512, 512), type=lambda x: tuple(map(int, x.split(','))),
                         help="Image shape, None for no resize")
 
     args = parser.parse_args()
--- util.py
+++ util.py
@@ -1,7 +1,9 @@
 import numpy as np
 from skimage import img_as_ubyte
 from skimage.transform import resize
+import torchaudio
 import imageio
+import cv2, os
 
 def bb_intersection_over_union(boxA, boxB):
     xA = max(boxA[0], boxB[0])
@@ -34,7 +36,7 @@ def compute_aspect_preserved_bbox(bbox, increase_area):
     left, top, right, bot = bbox
     width = right - left
     height = bot - top
- 
+
     width_increase = max(increase_area, ((1 + 2 * increase_area) * height - width) / (2 * width))
     height_increase = max(increase_area, ((1 + 2 * increase_area) * width - height) / (2 * height))
 
@@ -49,15 +51,13 @@ def compute_increased_bbox(bbox, increase_area):
     left, top, right, bot = bbox
     width = right - left
     height = bot - top
- 
+
     left = int(left - increase_area * width)
     top = int(top - increase_area * height)
     right = int(right + increase_area * width)
     bot = int(bot + increase_area * height)
 
     return (left, top, right, bot)
-
-
 
 def crop_bbox_from_frames(frame_list, tube_bbox, min_frames=16, image_shape=(256, 256), min_size=200,
                           increase_area=0.1, aspect_preserving=True):
@@ -71,18 +71,18 @@ def crop_bbox_from_frames(frame_list, tube_bbox, min_frames=16, image_shape=(256
     # Filter if it is too small
     if max(width, height) < min_size:
         return None, None
-    
+
     if aspect_preserving:
         left, top, right, bot = compute_aspect_preserved_bbox(tube_bbox, increase_area)
     else:
         left, top, right, bot = compute_increased_bbox(tube_bbox, increase_area)
- 
+
     # Compute out of bounds
     left_oob = -min(0, left)
     right_oob = right - min(right, frame_shape[1])
     top_oob = -min(0, top)
     bot_oob = bot - min(bot, frame_shape[0])
-    
+
     #Not use near the border
     if max(left_oob / float(width), right_oob / float(width), top_oob  / float(height), bot_oob / float(height)) > 0:
         return [None, None]
@@ -92,7 +92,7 @@ def crop_bbox_from_frames(frame_list, tube_bbox, min_frames=16, image_shape=(256
         out = [img_as_ubyte(resize(frame, image_shape, anti_aliasing=True)) for frame in selected]
     else:
         out = selected
- 
+
     return out, [left, top, right, bot]
 
 from multiprocessing import Pool
@@ -113,17 +113,35 @@ def scheduler(data_list, fn, args):
             f.flush()
     f.close()
 
-def save(path, frames, format):
+def write_frames_to_video(frame_list, output_file, fps, ffmpeg, remove, desired_fps):
+    height, width, channels = frame_list[0].shape
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the video codec (e.g., 'XVID', 'MJPG', 'mp4v')
+    video_writer = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    # Write each frame to the video
+    for frame in frame_list:
+        video_writer.write(frame)
+
+    # Release the video writer and destroy any remaining windows
+    video_writer.release()
+    os.system(ffmpeg.format(output_file, 'r', desired_fps, output_file.replace('.mp4', '-r.mp4')))
+    os.system(remove.format(output_file))
+
+def write_audio(audio_list, output_file, samplerate, ffmpeg, remove, desired_sample_rate):
+    # write torchaudio tensor to wav file
+    torchaudio.save(output_file, audio_list, samplerate)
+    os.system(ffmpeg.format(output_file, 'ar', desired_sample_rate,  output_file.replace('.wav', '-r.wav'))) 
+    os.system(remove.format(output_file))
+
+def save(path, frames, audio, format, fps, samplerate, desired_fps, desired_sample_rate):
+    ffmpeg_command = "ffmpeg -y -i {} -{} {} {}"
+    remove_command = "rm {}"
     if format == '.mp4':
-        imageio.mimsave(path, frames)
-    elif format == '.png':
-        if os.path.exists(path):
-            print ("Warning: skiping video %s" % os.path.basename(path))
-            return
-        else:
-            os.makedirs(path)
-        for j, frame in enumerate(frames):
-            imageio.imsave(os.path.join(path, str(j).zfill(7) + '.png'), frames[j]) 
+        write_frames_to_video(frames, path, fps, ffmpeg_command, remove_command,  desired_fps)
+        # write_audio(audio, path.replace('.mp4', '.wav'), samplerate, ffmpeg_command, remove_command, desired_sample_rate)
+        
     else:
         print ("Unknown format %s" % format)
         exit()
